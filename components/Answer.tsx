5cce3bb4c7c7f399a5449d62515585117c52e152
Sunil Band <sunilband0007@gmail.com>
2023-03-27 23:55:06 +0530

    removed possible undefined value error

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -9,7 +9,13 @@ const Answer = (props: Props) => {
   const [input,setInput]=useState("")
   const [answer,setAnswer]=useState("")
   const [scripture,setScripture]=useState("Bhagavad Gita")
-  const [loading,setLoading]=useState(false)
+  
+
+  const openAi = new OpenAIApi(
+    new Configuration({
+      apiKey:"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"
+    })
+  )
 
   const setterInput=async(e:any)=>{
     setInput(e.target.value)
@@ -17,19 +23,16 @@ const Answer = (props: Props) => {
 
   const buttonClick=async()=>{
     setAnswer("Loading...")
-    const response = await openAi.createChatCompletion({
+    const response:any = await openAi.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: input+scripture }],
     })
-    setAnswer(await response.data.choices[0].message.content)
+    if (response.data.choices[0].message.content != undefined) {
+      setAnswer(response.data.choices[0].message.content)  
+    } 
   }
-  
 
-  const openAi = new OpenAIApi(
-    new Configuration({
-      apiKey:"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"
-    })
-  )
+  
 
   const dropdown=(e:any)=>{
     setScripture(` according to ${e.target.value}? also give references`)
@@ -43,17 +46,17 @@ const Answer = (props: Props) => {
       {/* answer field */}
     <textarea className='w-screen self-center mt-16 border-2 max-w-7xl h-72 rounded-2xl p-4 flex flex-wrap bg-[#40414f] text-white text-justify font-medium tracking-wide overflow-y-scroll no-scrollbar'  value={answer} />
     {/* loader */}
-    {loading==true?<h3 className='text-center mt-4'>Creating response...</h3>:<></>}
+    
     {/* input fiend */}
       <input type="text" className='self-center  border-2 m-4 p-4 rounded-2xl bg-[#40414f] text-white w-4/5 tracking-wide' value={input} onChange={setterInput}/>
 
         {/*Dropdown  */}
-      <form className="w-52 self-center">
+      <form className="w-72 self-center">
             <select className="w-full p-2.5 text-white bg-[#202123] rounded-md shadow-sm outline-none appearance-none focus:border-[#202123] tracking-widest"  onChange={dropdown}>
-                <option className='text-center tracking-widest'>Bhagavad Gita</option>
-                <option className='text-center tracking-widest'>Holy Quran</option>
-                <option className='text-center tracking-widest'>Holy Bible</option>
-                <option className='text-center tracking-widest'>Guru Granth Sahib</option>
+                <option className='text-center tracking-widest'>Bhagavad Gita/Hinduism</option>
+                <option className='text-center tracking-widest'>Holy Quran/Islam</option>
+                <option className='text-center tracking-widest'>Holy Bible/Christanity</option>
+                <option className='text-center tracking-widest'>Guru Granth Sahib/Sikhism</option>
                 <option className='text-center tracking-widest'>Jainism</option>    
             </select>
         </form>
