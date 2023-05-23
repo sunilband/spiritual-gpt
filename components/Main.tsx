29984bd50075d196565e7660fc12f0c0e3a78496
Sunil Band <sunilband0007@gmail.com>
2023-05-23 11:15:20 +0530

    server updated with websockets and env fixed

--- components/Main.tsx
+++ components/Main.tsx
@@ -11,8 +11,8 @@ import  io from "socket.io-client";
 
 
 type Props = {};
-// const apiServer = "https://spiritual-gpt-api.onrender.com";
-const apiServer = "http://localhost:5000/";
+const apiServer = "https://spiritual-gpt-api.onrender.com";
+// const apiServer = "http://localhost:5000/";
   const socket=io(apiServer ,{
     transports: ['websocket'], 
     upgrade: false
