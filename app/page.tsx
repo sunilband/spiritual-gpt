4fe595056ede7591947db52556f473404cc4b2db
Sunil Band <sunilband0007@gmail.com>
2023-07-14 16:13:08 +0530

    moved metadata from layout to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Logo from '@/components/Logo/Logo'
 import { Main } from '../components/MainSection/Main'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Spiritual GPT',
+  description: 'Get spiiitual answers with the power of AI',
+}
 
 export default function Home() {
   return (
